3e6f425f3f3c83885ed68246854cc5d2eafcde6f
Shreyansh Pandey <shreyansh.fractal@gmail.com>
2024-02-11 00:50:44 +0530

    server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://collaborate-gules.vercel.app/',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
