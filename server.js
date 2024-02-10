850fc456c4c48f661265cced4dfabe81f2fb6b40
Shreyansh Pandey <shreyansh.fractal@gmail.com>
2024-02-11 00:19:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+io.on('connection', (socket) => {
+  console.log(`connected ${socket.id}`);
+  socket.on('message', (evt) => {
+      socket.broadcast.emit('message', evt)
+  })
+})
+io.on('disconnect', (evt) => {
+  log('some people left')
+})
+
+server.listen(4000, () => 'Server is running on port 3000');
